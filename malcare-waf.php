7805798efce3d548742843fa08f9419132db3a77
Ardi <ardata.indonesia@gmail.com>
2024-05-19 13:22:21 +0700

    cba

---
+++ malcare-waf.php
@@ -0,0 +1,9 @@
+<?php
+// Please validate auto_prepend_file setting before removing this file
+
+if (file_exists('/home/jawa2439/public_html/servicepintuotomatis.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php')) {
+	define("MCDATAPATH", '/home/jawa2439/public_html/servicepintuotomatis.com/wp-content/mc_data/');
+	define("MCCONFKEY", '65add9d085b50ad973784782bfa1bfd1');
+	include_once('/home/jawa2439/public_html/servicepintuotomatis.com/wp-content/plugins/malcare-security/protect/prepend/ignitor.php');
+}
+?>
